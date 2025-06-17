e3be0689777d87ac68562bc814c4341343a491b3
Nikhil Panchal <nikhilp.ind27@gmail.com>
2025-06-17 13:19:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["https://preview--perfect-ui-for-chatbot.lovable.app","http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
